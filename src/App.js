e740cbf21efd57bba4d4f3a7b84587d26a34324b
unknown <pjh00032@gmail.com>
2021-08-26 17:56:32 +0900

    scroll add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React from "react";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import InfiniteScroll from 'react-infinite-scroll-component';
 export { RiHeart3Fill } from 'react-icons/ri';
 
 /**
@@ -13,13 +14,17 @@ class App extends React.Component{
             events : [{
                 date : "22/09/2017", /*기준일*/
                 dateString: "22/09/2017 11:00:11", /*입력된 시간*/
-                event: "start timeline", /*코멘트*/
+                event: "timeline(material ui) + infinite scroll(react-infinite-scroll-component)", /*코멘트*/
                 color : "#fcd804" /*아이콘 색상*/
             }
             ],
             inputText : 0 /*input text value*/
+            , hasMore: true
         }
+    }
 
+    componentDidMount(){
+        this.onScrollAddTimeLine(5);
     }
 
     /*
@@ -77,6 +82,56 @@ class App extends React.Component{
         }
     }
 
+    onScrollAddTimeLine = (addNumber) => {
+        let time = null;
+        let today = new Date();
+        let date = ''; /*기준일*/
+        let addDay = '';
+        let color = '';
+
+        //입력한 값이 숫자일때만 동작
+        if(!isNaN(addNumber)){
+            let newEvents = []; //추가할 timeline
+            let num = 1;
+
+            while(num<=addNumber) {
+                if(num%2 === 0){
+                    //노란색
+                    color = "#fcd804";
+                    time = new Date(today.setTime(today.getTime()+1));
+                }
+                else
+                {
+                    //초록색
+                    color = "#0aaf0c";
+                    time = new Date(today.setDate(today.getDate()+1));
+                }
+
+                date = time.getDate() + "/" + (time.getMonth()+1) + "/" + time.getFullYear();
+                addDay = time.getDate() + "/" + (time.getMonth()+1) + "/" + time.getFullYear() + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + ":" + time.getMilliseconds();
+
+                newEvents.push({
+                    date : date,
+                    dateString: addDay,
+                    event: num,
+                    color: color
+                })
+
+                num++;
+            }
+
+
+            //적용
+            const { events } = this.state;
+            this.setState({
+                events: events.concat(newEvents)
+            })
+        }else
+        {
+            alert("숫자를 입력해주시기 바랍니다.");
+        }
+    }
+
     //input text onChange Evnet
     onInputTextChange = (e) => {
         this.setState({inputText : e.target.value})
@@ -89,13 +144,25 @@ class App extends React.Component{
         }
     }
 
+    fetchMoreData = () => {
+        if (this.state.events.length >= 100) {
+          this.setState({ hasMore: false });
+          return;
+        }
+        // a fake async api call like which sends
+        // 20 more records in .5 secs
+        setTimeout(() => {
+            this.onScrollAddTimeLine(20);
+        }, 500);
+      };
+
     render() {
         let addDateComment = "12/8/2021"; /*시작 기준일*/
 
         return (
             <div>
                 {/**timeline 추가 input************************************************************************************************************************/}
-                <div style={{height:100}}>
+                {/* <div style={{height:100}}>
                     <span>
                         Enter the number to add of timeline :&nbsp;
                     </span>
@@ -103,7 +170,7 @@ class App extends React.Component{
                     <input type={"number"} value = {this.state.inputText} onChange={this.onInputTextChange} maxLength={5} onInput={this.onInputMaxLengthCheck}/>
                     &nbsp;
                     <button onClick={this.onAddTimeLine}> add timeline </button>
-                </div>
+                </div> */}
 
                 {/**timeline 표출*******************************************************************************************************************************/}
                 <div
@@ -112,50 +179,27 @@ class App extends React.Component{
                 >
                     <h1>Timeline Test</h1>
                     <VerticalTimeline layout="1-column">
-                        <VerticalTimelineElement
-                            icon={ <div>Waiting to accept</div> }
-                            iconStyle={{ background: "#fcd804", width:120, left:-42, borderRadius:2, height:25, lineHeight:2 }}
-                        >
-                            <h3>가나다라</h3>
-
-                        </VerticalTimelineElement>
-
-                        {this.state.events.map((event) => {
-
-                            //기준일이 같으면 timeline만 추가
-                            if(addDateComment === event.date){
-                                return (
-                                    <VerticalTimelineElement
-                                        date={event.date}
-                                        icon={ <div></div> }
-                                        iconStyle={{ background: event.color}}
-                                    >
-                                        <h3
-                                            className="vertical-timeline-element-title"
-                                            dangerouslySetInnerHTML={{ __html: event.event }}
-                                        />
-                                        <button
-                                            onClick={function () {
-                                                alert(event.date);
-                                            }}
-                                        >show date</button>
-                                    </VerticalTimelineElement>
-                                )
+                        <InfiniteScroll
+                            dataLength={this.state.events.length}
+                            next={this.fetchMoreData}
+                            hasMore={this.state.hasMore}
+                            loader={<h4>Loading...</h4>}
+                            height={850}
+                            endMessage={
+                            <p style={{ textAlign: "center" }}>
+                                <b>Yay! You have seen it all</b>
+                            </p>
                             }
-                            else //기준일이 다르면, 기준일 추가, timeline 추가
-                            {
-                                addDateComment = event.date;
-                                return (
-                                    <div>
+                        >
+                            {this.state.events.map((event) => {
+
+                                //기준일이 같으면 timeline만 추가
+                                if(addDateComment === event.date){
+                                    return (
                                         <VerticalTimelineElement
-                                            icon={ <div>{event.date}</div> }
-                                            iconStyle={{ background: "#a1a1a0", color:"#ffffff", width:120, left:-42, borderRadius:2, height:25, lineHeight:2 }}
-                                        >
-                                        </VerticalTimelineElement>
-                                        <VerticalTimelineElement
-                                            date={event.dateString}
+                                            date={event.date}
                                             icon={ <div></div> }
-                                            iconStyle={{ background: event.color}}
+                                            iconStyle={{ background: event.color, left:45}}
                                         >
                                             <h3
                                                 className="vertical-timeline-element-title"
@@ -163,15 +207,43 @@ class App extends React.Component{
                                             />
                                             <button
                                                 onClick={function () {
-                                                    alert(event.dateString);
+                                                    alert(event.date);
                                                 }}
                                             >show date</button>
                                         </VerticalTimelineElement>
-                                    </div>
-                                )
+                                    )
+                                }
+                                else //기준일이 다르면, 기준일 추가, timeline 추가
+                                {
+                                    addDateComment = event.date;
+                                    return (
+                                        <div>
+                                            <VerticalTimelineElement
+                                                icon={ <div>{event.date}</div> }
+                                                iconStyle={{ background: "#a1a1a0", color:"#ffffff", width:120, borderRadius:2, height:25, lineHeight:2 }}
+                                            >
+                                            </VerticalTimelineElement>
+                                            <VerticalTimelineElement
+                                                date={event.dateString}
+                                                icon={ <div></div> }
+                                                iconStyle={{ background: event.color, left:45}}
+                                            >
+                                                <h3
+                                                    className="vertical-timeline-element-title"
+                                                    dangerouslySetInnerHTML={{ __html: event.event }}
+                                                />
+                                                <button
+                                                    onClick={function () {
+                                                        alert(event.dateString);
+                                                    }}
+                                                >show date</button>
+                                            </VerticalTimelineElement>
+                                        </div>
+                                    )
 
-                            }
-                        })}
+                                }
+                                })}
+                        </InfiniteScroll>
 
                     </VerticalTimeline>
                 </div>
